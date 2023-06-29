6f716e2dcd9ea0498a6461ef8b14bf1ce2e94b69
Hernan Camilo Rivera Arteaga <camilorivera108@gmail.com>
2023-06-28 23:51:54 -0500

    FixOB: Configuracion de authenticacion con DJ rest auth

--- config/settings/base.py
+++ config/settings/base.py
@@ -311,8 +311,8 @@ SOCIALACCOUNT_FORMS = {"signup": "acarreapp.users.forms.UserSocialSignupForm"}
 # django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
